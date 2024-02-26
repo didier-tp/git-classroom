9840b3e747d7deffc37c4b1d5cb37092743faf17
DidierDefrance <didier@d-defrance.fr>
2024-02-26 15:58:02 +0100

    modif3 depuis intelliJ

--- basicJava/src/main/java/tp/Main.java
+++ basicJava/src/main/java/tp/Main.java
@@ -3,7 +3,7 @@ package tp;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hello world from basicJava ##2");
+		System.out.println("hello world from basicJava ##3");
 
 	}
 
