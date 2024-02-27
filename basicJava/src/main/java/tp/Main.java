cb1df75590d2a16e63a8f47e3328bb8d6f5f93dd
DidierTp_dev_x <xyz.d2f.defrance@gmail.com>
2024-02-27 11:46:45 +0100

    v4_14

--- basicJava/src/main/java/tp/Main.java
+++ basicJava/src/main/java/tp/Main.java
@@ -3,8 +3,8 @@ package tp;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hello world from basicJava <<14bis>>");
-		System.out.println("ma suite v3");
+		System.out.println("hello world from basicJava <<15>>");
+		System.out.println("ma suite v4");
 
 	}
 
