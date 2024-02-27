b943b79735813b1747aaeec10c7074fc6ba57473
DidierTp_dev_x <xyz.d2f.defrance@gmail.com>
2024-02-27 12:48:57 +0100

    MyUtil.displayDlg

--- basicJava/src/main/java/tp/Main.java
+++ basicJava/src/main/java/tp/Main.java
@@ -7,6 +7,7 @@ public class Main {
 		System.out.println("ma suite v6 ..");
 		
 		MyUtil.display("coucou");
+		//MyUtil.displayDlg("coucou2");
 	}
 
 }
--- basicJava/src/main/java/tp/MyUtil.java
+++ basicJava/src/main/java/tp/MyUtil.java
@@ -1,7 +1,13 @@
 package tp;
 
+import javax.swing.JOptionPane;
+
 public class MyUtil {
 	public static void display(String message){
 		System.out.println(message);
+	}
+	
+	public static void displayDlg(String message){
+		JOptionPane.showMessageDialog(null, message);
 	}
 }
