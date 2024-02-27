9e9358bb01ad3c57133b34e1722900f157983e69
DidierTp_dev_z <xyz.defrance@gmail.com>
2024-02-27 11:47:10 +0100

    suite v5 depuis intelliJ

--- basicJava/src/main/java/tp/Main.java
+++ basicJava/src/main/java/tp/Main.java
@@ -3,8 +3,8 @@ package tp;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hello world from basicJava <<14bis>>");
-		System.out.println("ma suite v3");
+		System.out.println("hello world from basicJava <<14bisbis>>");
+		System.out.println("ma suite v5");
 
 	}
 
