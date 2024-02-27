039e917d0144b45afe24ff2bf89fe72a46d152f8
DidierTp_dev_x <xyz.d2f.defrance@gmail.com>
2024-02-27 11:30:21 +0100

    suite v2

--- basicJava/src/main/java/tp/Main.java
+++ basicJava/src/main/java/tp/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("hello world from basicJava <<14>>");
-		System.out.println("ma suite");
+		System.out.println("ma suite 2");
 	}
 
 }
