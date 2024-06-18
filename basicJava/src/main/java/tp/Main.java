e8f989f4a12e366c4f7d2212a45277bc89a0a79a
TestUser <testuser@test.com>
2024-06-18 12:55:22 +0100

    Modif by TestUser

--- basicJava/src/main/java/tp/Main.java
+++ basicJava/src/main/java/tp/Main.java
@@ -5,6 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("HELLO world &&&");
 		System.out.println("Coucou !");
+		
+		String test = "new String";
+		System.out.println(test);
 	}
 
 }
