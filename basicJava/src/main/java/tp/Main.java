0242faaca831661087086da58274d8e7aae02324
DidierTp_dev_x <dev@tp.fr>
2024-06-18 13:01:30 +0100

    TC push

--- basicJava/src/main/java/tp/Main.java
+++ basicJava/src/main/java/tp/Main.java
@@ -3,7 +3,7 @@ package tp;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("HELLO world !!!");
+		System.out.println("HELLO woooooorld !!!");
 		System.out.println("Coucou Laurent!");
 		
 		String test = "new String";
