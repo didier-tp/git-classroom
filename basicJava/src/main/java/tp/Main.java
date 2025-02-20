5cdbe3312be77dbe5c23848f9fba30347a01af9e
HanenAdhar <HanenAdhar@gmail.com>
2025-02-20 11:59:37 +0100

    changer java main

--- basicJava/src/main/java/tp/Main.java
+++ basicJava/src/main/java/tp/Main.java
@@ -3,7 +3,7 @@ package tp;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("HELLO woooooorld !!!");
+		System.out.println("HELLO Hanouna !!!");
 		System.out.println("Coucou Laurent!");
 		System.out.println("Coucou Franck!");
 		String test = "new String";
