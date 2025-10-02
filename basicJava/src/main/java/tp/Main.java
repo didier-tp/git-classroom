189507a42b21c0ab71b12da00be97608365ea98d
RIO Mevenig <mevenig.rio@dgfip.finances.gouv.fr>
2025-10-02 16:47:06 +0200

    hello wolrd en mieux

--- basicJava/src/main/java/tp/Main.java
+++ basicJava/src/main/java/tp/Main.java
@@ -3,7 +3,7 @@ package tp;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("HELLO from java !!!");
+		System.out.println("HELLO world from java !!!");
 		String test = "new String";
 		System.out.println(test);
 
